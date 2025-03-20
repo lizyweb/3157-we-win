eea66fda5f0cd74b1155df62c7989263a9b78ef2
lizyweb <147726740+lizyweb@users.noreply.github.com>
2025-03-20 17:53:19 +0530

    Add files via upload

---
+++ js/main-5.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+  
+       // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
+});
